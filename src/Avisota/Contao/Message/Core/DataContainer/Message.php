7af09583f0eeba1b449784a74e231894ce56cccf
Sven Baumann <baumann.sv@gmail.com>
2015-07-28 18:17:06 +0200

    reform code

--- src/Avisota/Contao/Message/Core/DataContainer/Message.php
+++ src/Avisota/Contao/Message/Core/DataContainer/Message.php
@@ -31,168 +31,163 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Message implements EventSubscriberInterface
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public static function getSubscribedEvents()
-	{
-		return array(
-			GetGroupHeaderEvent::NAME . '[orm_avisota_message]'        => 'getGroupHeader',
-			ParentViewChildRecordEvent::NAME . '[orm_avisota_message]' => 'parentViewChildRecord',
-		);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            GetGroupHeaderEvent::NAME . '[orm_avisota_message]'        => 'getGroupHeader',
+            ParentViewChildRecordEvent::NAME . '[orm_avisota_message]' => 'parentViewChildRecord',
+        );
+    }
 
-	public function addHeader($add, $dc)
-	{
-		// TODO refactore for DCG
-		return;
+    public function addHeader($add, $dc)
+    {
+        // TODO refactore for DCG
+        return;
 
-		$newsletterCategoryRepository = EntityHelper::getRepository('Avisota\Contao:MessageCategory');
-		/** @var \Avisota\Contao\Entity\MessageCategory $newsletterCategory */
-		$newsletterCategory = $newsletterCategoryRepository->find($dc->id);
+        $newsletterCategoryRepository = EntityHelper::getRepository('Avisota\Contao:MessageCategory');
+        /** @var \Avisota\Contao\Entity\MessageCategory $newsletterCategory */
+        $newsletterCategory = $newsletterCategoryRepository->find($dc->id);
 
-		$key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['recipients'][0];
-		if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getRecipientsMode() != 'byMessage') {
-			$fallback = $newsletterCategory->getRecipientsMode() == 'byMessageOrCategory';
+        $key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['recipients'][0];
+        if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getRecipientsMode() != 'byMessage') {
+            $fallback = $newsletterCategory->getRecipientsMode() == 'byMessageOrCategory';
 
-			/** @var RecipientSource $recipientSource */
-			$recipientSource = $newsletterCategory->getRecipients();
-			if ($recipientSource) {
-				$add[$key] = sprintf(
-					'<a href="contao/main.php?do=avisota_recipient_source&act=edit&id=%d">%s</a>%s',
-					$recipientSource->getId(),
-					$recipientSource->getTitle(),
-					$fallback ? ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'] : ''
-				);
-			}
-			else {
-				unset($add[$key]);
-			}
-		}
-		else {
-			unset($add[$key]);
-		}
+            /** @var RecipientSource $recipientSource */
+            $recipientSource = $newsletterCategory->getRecipients();
+            if ($recipientSource) {
+                $add[$key] = sprintf(
+                    '<a href="contao/main.php?do=avisota_recipient_source&act=edit&id=%d">%s</a>%s',
+                    $recipientSource->getId(),
+                    $recipientSource->getTitle(),
+                    $fallback ? ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'] : ''
+                );
+            } else {
+                unset($add[$key]);
+            }
+        } else {
+            unset($add[$key]);
+        }
 
-		$key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['layout'][0];
-		if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getLayoutMode() != 'byMessage') {
-			$add[$key] = $newsletterCategory
-				->getLayout()
-				->getTitle();
-			if ($newsletterCategory->getLayoutMode() == 'byMessageOrCategory') {
-				$add[$key] .= ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'];
-			}
-		}
-		else {
-			unset($add[$key]);
-		}
+        $key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['layout'][0];
+        if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getLayoutMode() != 'byMessage') {
+            $add[$key] = $newsletterCategory
+                ->getLayout()
+                ->getTitle();
+            if ($newsletterCategory->getLayoutMode() == 'byMessageOrCategory') {
+                $add[$key] .= ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'];
+            }
+        } else {
+            unset($add[$key]);
+        }
 
-		$key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['queue'][0];
-		if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getQueueMode() != 'byMessage') {
-			$add[$key] = $newsletterCategory
-				->getQueue()
-				->getTitle();
-			if ($newsletterCategory->getQueueMode() == 'byMessageOrCategory') {
-				$add[$key] .= ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'];
-			}
-		}
-		else {
-			unset($add[$key]);
-		}
+        $key = $GLOBALS['TL_LANG']['orm_avisota_message_category']['queue'][0];
+        if (!$newsletterCategory->getBoilerplates() && $newsletterCategory->getQueueMode() != 'byMessage') {
+            $add[$key] = $newsletterCategory
+                ->getQueue()
+                ->getTitle();
+            if ($newsletterCategory->getQueueMode() == 'byMessageOrCategory') {
+                $add[$key] .= ' ' . $GLOBALS['TL_LANG']['orm_avisota_message']['fallback'];
+            }
+        } else {
+            unset($add[$key]);
+        }
 
-		return $add;
-	}
+        return $add;
+    }
 
-	public function getGroupHeader(GetGroupHeaderEvent $event)
-	{
-		/** @var EntityModel $model */
-		$model = $event->getModel();
-		/** @var \Avisota\Contao\Entity\Message $message */
-		$message = $model->getEntity();
+    public function getGroupHeader(GetGroupHeaderEvent $event)
+    {
+        /** @var EntityModel $model */
+        $model = $event->getModel();
+        /** @var \Avisota\Contao\Entity\Message $message */
+        $message = $model->getEntity();
 
-		if ($message->getCategory()->getBoilerplates()) {
-			$language = $message->getLanguage();
+        if ($message->getCategory()->getBoilerplates()) {
+            $language = $message->getLanguage();
 
-			if (isset($GLOBALS['TL_LANG']['LNG'][$language])) {
-				$language = $GLOBALS['TL_LANG']['LNG'][$language];
-			}
+            if (isset($GLOBALS['TL_LANG']['LNG'][$language])) {
+                $language = $GLOBALS['TL_LANG']['LNG'][$language];
+            }
 
-			$event->setValue($language);
-		}
-		else if ($model->getProperty('sendOn')) {
-			$parseDateEvent = new ParseDateEvent($message->getSendOn()->getTimestamp(), 'F Y');
+            $event->setValue($language);
+        } else {
+            if ($model->getProperty('sendOn')) {
+                $parseDateEvent = new ParseDateEvent($message->getSendOn()->getTimestamp(), 'F Y');
 
-			/** @var EventDispatcher $eventDispatcher */
-			$eventDispatcher = $GLOBALS['container']['event-dispatcher'];
-			$eventDispatcher->dispatch(ContaoEvents::DATE_PARSE, $parseDateEvent);
+                /** @var EventDispatcher $eventDispatcher */
+                $eventDispatcher = $GLOBALS['container']['event-dispatcher'];
+                $eventDispatcher->dispatch(ContaoEvents::DATE_PARSE, $parseDateEvent);
 
-			$event->setValue($parseDateEvent->getResult());
-		}
-		else {
-			$event->setValue($GLOBALS['TL_LANG']['orm_avisota_message']['notSend']);
-		}
-	}
+                $event->setValue($parseDateEvent->getResult());
+            } else {
+                $event->setValue($GLOBALS['TL_LANG']['orm_avisota_message']['notSend']);
+            }
+        }
+    }
 
-	/**
-	 * Add the recipient row.
-	 *
-	 * @param array
-	 */
-	public function parentViewChildRecord(ParentViewChildRecordEvent $event)
-	{
-		/** @var EntityModel $model */
-		$model = $event->getModel();
-		/** @var \Avisota\Contao\Entity\Message $message */
-		$message = $model->getEntity();
+    /**
+     * Add the recipient row.
+     *
+     * @param array
+     */
+    public function parentViewChildRecord(ParentViewChildRecordEvent $event)
+    {
+        /** @var EntityModel $model */
+        $model = $event->getModel();
+        /** @var \Avisota\Contao\Entity\Message $message */
+        $message = $model->getEntity();
 
-		if ($message->getCategory()->getBoilerplates()) {
-			$language = $message->getLanguage();
+        if ($message->getCategory()->getBoilerplates()) {
+            $language = $message->getLanguage();
 
-			if (isset($GLOBALS['TL_LANG']['LNG'][$language])) {
-				$language = $GLOBALS['TL_LANG']['LNG'][$language];
-			}
+            if (isset($GLOBALS['TL_LANG']['LNG'][$language])) {
+                $language = $GLOBALS['TL_LANG']['LNG'][$language];
+            }
 
-			$label = sprintf(
-				'%s [%s]',
-				$message->getSubject(),
-				$language
-			);
+            $label = sprintf(
+                '%s [%s]',
+                $message->getSubject(),
+                $language
+            );
 
-			$event->setHtml($label);
-		}
-		else {
-			$icon = $model->getProperty('sendOn') ? 'visible' : 'invisible';
+            $event->setHtml($label);
+        } else {
+            $icon = $model->getProperty('sendOn') ? 'visible' : 'invisible';
 
-			$label = $model->getProperty('subject');
+            $label = $model->getProperty('subject');
 
-			if ($message->getSendOn()) {
-				$parseDateEvent = new ParseDateEvent(
-					$message->getSendOn()->getTimestamp(),
-					$GLOBALS['TL_CONFIG']['datimFormat']
-				);
+            if ($message->getSendOn()) {
+                $parseDateEvent = new ParseDateEvent(
+                    $message->getSendOn()->getTimestamp(),
+                    $GLOBALS['TL_CONFIG']['datimFormat']
+                );
 
-				/** @var EventDispatcher $eventDispatcher */
-				$eventDispatcher = $GLOBALS['container']['event-dispatcher'];
-				$eventDispatcher->dispatch(ContaoEvents::DATE_PARSE, $parseDateEvent);
+                /** @var EventDispatcher $eventDispatcher */
+                $eventDispatcher = $GLOBALS['container']['event-dispatcher'];
+                $eventDispatcher->dispatch(ContaoEvents::DATE_PARSE, $parseDateEvent);
 
-				$label .= ' <span style="color:#b3b3b3; padding-left:3px;">(' . sprintf(
-						$GLOBALS['TL_LANG']['orm_avisota_message']['sended'],
-						$parseDateEvent->getResult()
-					) . ')</span>';
-			}
+                $label .= ' <span style="color:#b3b3b3; padding-left:3px;">(' . sprintf(
+                        $GLOBALS['TL_LANG']['orm_avisota_message']['sended'],
+                        $parseDateEvent->getResult()
+                    ) . ')</span>';
+            }
 
-			/** @var EventDispatcher $eventDispatcher */
-			$eventDispatcher = $GLOBALS['container']['event-dispatcher'];
-			$getThemeEvent   = new GetThemeEvent();
-			$eventDispatcher->dispatch(ContaoEvents::BACKEND_GET_THEME, $getThemeEvent);
+            /** @var EventDispatcher $eventDispatcher */
+            $eventDispatcher = $GLOBALS['container']['event-dispatcher'];
+            $getThemeEvent   = new GetThemeEvent();
+            $eventDispatcher->dispatch(ContaoEvents::BACKEND_GET_THEME, $getThemeEvent);
 
-			$event->setHtml(
-				sprintf(
-					'<div class="list_icon" style="background-image:url(\'system/themes/%s/images/%s.gif\');">%s</div>',
-					$getThemeEvent->getTheme(),
-					$icon,
-					$label
-				)
-			);
-		}
-	}
+            $event->setHtml(
+                sprintf(
+                    '<div class="list_icon" style="background-image:url(\'system/themes/%s/images/%s.gif\');">%s</div>',
+                    $getThemeEvent->getTheme(),
+                    $icon,
+                    $label
+                )
+            );
+        }
+    }
 }
--- src/Avisota/Contao/Message/Core/DataContainer/MessageContent.php
+++ src/Avisota/Contao/Message/Core/DataContainer/MessageContent.php
@@ -26,89 +26,88 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class MessageContent implements EventSubscriberInterface
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public static function getSubscribedEvents()
-	{
-		return array(
-			GetGroupHeaderEvent::NAME . '[orm_avisota_message_content]'        => 'getGroupHeader',
-			ParentViewChildRecordEvent::NAME . '[orm_avisota_message_content]' => 'parentViewChildRecord',
-		);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            GetGroupHeaderEvent::NAME . '[orm_avisota_message_content]'        => 'getGroupHeader',
+            ParentViewChildRecordEvent::NAME . '[orm_avisota_message_content]' => 'parentViewChildRecord',
+        );
+    }
 
-	/**
-	 * Return the send button
-	 *
-	 * @param array
-	 * @param string
-	 * @param string
-	 * @param string
-	 * @param string
-	 * @param string
-	 *
-	 * @return string
-	 */
-	public function sendMessageButton($href, $label, $title, $icon, $attributes)
-	{
-		$user = \BackendUser::getInstance();
+    /**
+     * Return the send button
+     *
+     * @param array
+     * @param string
+     * @param string
+     * @param string
+     * @param string
+     * @param string
+     *
+     * @return string
+     */
+    public function sendMessageButton($href, $label, $title, $icon, $attributes)
+    {
+        $user = \BackendUser::getInstance();
 
-		if (!($user->isAdmin || $user->hasAccess('send', 'avisota_newsletter_permissions'))) {
-			$label = $GLOBALS['TL_LANG']['orm_avisota_message']['view_only'][0];
-			$title = $GLOBALS['TL_LANG']['orm_avisota_message']['view_only'][1];
-		}
-		return ' &#160; :: &#160; <a href="' . $this->addToUrl(
-			$href . '&amp;id=' . $this->Input->get('id')
-		) . '" title="' . specialchars($title) . '"' . $attributes . ' class="header_send">' . $label . '</a> ';
-	}
+        if (!($user->isAdmin || $user->hasAccess('send', 'avisota_newsletter_permissions'))) {
+            $label = $GLOBALS['TL_LANG']['orm_avisota_message']['view_only'][0];
+            $title = $GLOBALS['TL_LANG']['orm_avisota_message']['view_only'][1];
+        }
+        return ' &#160; :: &#160; <a href="' . $this->addToUrl(
+            $href . '&amp;id=' . $this->Input->get('id')
+        ) . '" title="' . specialchars($title) . '"' . $attributes . ' class="header_send">' . $label . '</a> ';
+    }
 
-	public function getGroupHeader(GetGroupHeaderEvent $event)
-	{
-		$model = $event->getModel();
-		$cell  = $model->getProperty('cell');
+    public function getGroupHeader(GetGroupHeaderEvent $event)
+    {
+        $model = $event->getModel();
+        $cell  = $model->getProperty('cell');
 
-		if (isset($GLOBALS['TL_LANG']['orm_avisota_message_content']['cells'][$cell])) {
-			$cell = $GLOBALS['TL_LANG']['orm_avisota_message_content']['cells'][$cell];
-		}
+        if (isset($GLOBALS['TL_LANG']['orm_avisota_message_content']['cells'][$cell])) {
+            $cell = $GLOBALS['TL_LANG']['orm_avisota_message_content']['cells'][$cell];
+        }
 
-		$event->setValue($cell);
-	}
+        $event->setValue($cell);
+    }
 
-	/**
-	 * Add the recipient row.
-	 *
-	 * @param array
-	 */
-	public function parentViewChildRecord(ParentViewChildRecordEvent $event)
-	{
-		/** @var MessageRendererInterface $renderer */
-		$renderer = $GLOBALS['container']['avisota.message.renderer'];
+    /**
+     * Add the recipient row.
+     *
+     * @param array
+     */
+    public function parentViewChildRecord(ParentViewChildRecordEvent $event)
+    {
+        /** @var MessageRendererInterface $renderer */
+        $renderer = $GLOBALS['container']['avisota.message.renderer'];
 
-		/** @var EntityModel $model */
-		$model = $event->getModel();
-		/** @var \Avisota\Contao\Entity\MessageContent $content */
-		$content = $model->getEntity();
+        /** @var EntityModel $model */
+        $model = $event->getModel();
+        /** @var \Avisota\Contao\Entity\MessageContent $content */
+        $content = $model->getEntity();
 
-		$key = $content->getInvisible() ? 'unpublished' : 'published';
+        $key = $content->getInvisible() ? 'unpublished' : 'published';
 
-		try {
-			$element = $renderer->renderContent($content);
-		}
-		catch (\Exception $exception) {
-			$element = sprintf(
-				"<span style=\"color:red\">%s</span>",
-				$exception->getMessage()
-			);
-		}
+        try {
+            $element = $renderer->renderContent($content);
+        } catch (\Exception $exception) {
+            $element = sprintf(
+                "<span style=\"color:red\">%s</span>",
+                $exception->getMessage()
+            );
+        }
 
-		/** @var EntityAccessor $entityAccessor */
-		$entityAccessor = $GLOBALS['container']['doctrine.orm.entityAccessor'];
+        /** @var EntityAccessor $entityAccessor */
+        $entityAccessor = $GLOBALS['container']['doctrine.orm.entityAccessor'];
 
-		$context            = $entityAccessor->getProperties($content);
-		$context['key']     = $key;
-		$context['element'] = $element;
+        $context            = $entityAccessor->getProperties($content);
+        $context['key']     = $key;
+        $context['element'] = $element;
 
-		$template = new \TwigTemplate('avisota/backend/mce_element', 'html5');
-		$event->setHtml($template->parse($context));
-	}
+        $template = new \TwigTemplate('avisota/backend/mce_element', 'html5');
+        $event->setHtml($template->parse($context));
+    }
 }
