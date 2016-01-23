72b4500ae3dab039ab04db43f1c281121816cac1
Sven Baumann <baumann.sv@gmail.com>
2016-01-23 13:12:33 +0100

    make travis happy add clean phpunit test

---
+++ tests/Test/Module/ListingTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright © 2016 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @copyright  way.vision 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @package    avisota/contao-core
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+namespace Avisota\Test\Contao\Message\Core\Module;
+
+/**
+ * Class ListingTest
+ *
+ * @package Avisota\Test\Contao\Message\Core\Module
+ */
+class ListingTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCompile()
+    {
+    }
+}
