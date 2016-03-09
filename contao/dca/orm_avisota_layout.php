6fd1967796155813bf6b097f67ffe054064e6ac9
Sven Baumann <baumann.sv@gmail.com>
2016-03-09 17:52:10 +0100

    give layout view edit all

--- contao/dca/orm_avisota_layout.php
+++ contao/dca/orm_avisota_layout.php
@@ -87,15 +87,14 @@ $GLOBALS['TL_DCA']['orm_avisota_layout'] = array
             'format' => '%s'
         ),
         'global_operations' => array
-        (/*
-			'all' => array
-			(
-				'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
-				'href'       => 'act=select',
-				'class'      => 'header_edit_all',
-				'attributes' => 'onclick="Backend.getScrollOffset();" accesskey="e"'
-			)
-			*/
+        (
+            'all' => array
+            (
+                'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'       => 'act=select',
+                'class'      => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+            )
         ),
         'operations'        => array
         (
